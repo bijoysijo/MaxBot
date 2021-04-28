edcb86ee2d31f528ed33e14961cfd9ae660ba4a7
Bijoy Sijo <bijoysijo21@gmail.com>
2021-04-29 02:27:06 +0530

    basic working setup with client conn.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from dotenv import load_dotenv
 
 # load env variables from .env file
@@ -6,10 +7,10 @@ load_dotenv()
 
 class MyClient(discord.Client):
   async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
+    print('Logged in as {0}!'.format(self.user))
 
   async def on_message(self, message):
     print('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
-client.run('TOKEN')
+client.run(os.getenv('TOKEN', 'Token not found'))
