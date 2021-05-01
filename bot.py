0f8674413ebfe35daa9aaf2ce769f6bb790ec998
Bijoy Sijo <bijoysijo21@gmail.com>
2021-05-01 06:07:16 +0530

    add feature to respond when a user reacts on a message

--- bot.py
+++ bot.py
@@ -23,4 +23,12 @@ async def on_member_join(member):
   channel = bot.get_channel(782892132959518732)
   await channel.send(f"Heya! {member.name}, Welcome to {member.guild.name}.")
 
+@bot.event
+async def on_raw_reaction_add(payload):
+  # print(f"channel_id: {payload.channel_id}, emoji: {payload.emoji}, event_type: {payload.event_type}, guild_id: {payload.guild_id}, member: {payload.member}, message_id: {payload.message_id}, user_id: {payload.user_id}")
+  channel = bot.get_channel(payload.channel_id)
+  message = await channel.fetch_message(payload.message_id)
+  message_author = message.author
+  await channel.send(f"{payload.member} just reacted to a message from {message_author}")
+
 bot.run(os.getenv('TOKEN', 'Token not found'))
